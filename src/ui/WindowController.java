22d28698c4f781d4759af646cb78e14459cb8b64
crojash <crojasherrera1@gmail.com>
2022-09-30 09:45:39 -0400

    window update

--- src/ui/WindowController.java
+++ src/ui/WindowController.java
@@ -1,5 +1,19 @@
 package ui;
+import javafx.fxml.FXML;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextArea;
 
-public class WindowController {
+public class WindowController{
+
+    @FXML
+    private TableColumn<?, ?> artist;
+
+    @FXML
+    private TableColumn<?, ?> name;
+    
+    
+    private void populateSongView() {
+    	
+    }
 
 }
