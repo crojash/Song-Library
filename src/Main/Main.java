ea6edc4aba6e5b615381302f123fdcb28e28aa9d
crojash <crojasherrera1@gmail.com>
2022-09-30 09:20:10 -0400

    WIndow update

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,10 +1,31 @@
 package Main;
 
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application{
+
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(Main.class.getResource("/ui/Window.fxml"));
+			//root.getStylesheets().add("/application/test.css");
+			//primaryStage.initStyle(StageStyle.UNDECORATED);
+			//primaryStage.initModality(Modality.APPLICATION_MODAL);
+			primaryStage.setScene(new Scene(root));
+			primaryStage.setTitle("Song Library");
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		//TES
+		launch(args);
 	}
 
 }
