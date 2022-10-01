58c9e6776abd000c35d143cd8b26d064c808d97e
crojash <crojasherrera1@gmail.com>
2022-10-01 11:26:20 -0400

    fxml file connected to controller.

--- src/ui/WindowController.java
+++ src/ui/WindowController.java
@@ -1,22 +1,16 @@
 package ui;
+
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextArea;
 
-public class WindowController{
-
-	@FXML
-    private Button addB;
+public class WindowController {
 
     @FXML
     private TableColumn<?, ?> artist;
 
-    @FXML
-    private Button deleteB;
-
-    @FXML
-    private Button editB;
 
     @FXML
     private TableColumn<?, ?> name;
@@ -24,9 +18,19 @@ public class WindowController{
     @FXML
     private TextArea textArea;
 
-    
-    private void populateSongView() {
-    	
+    @FXML
+    void AddOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void deleteOnAction(ActionEvent event) {
+
+    }
+
+    @FXML
+    void editOnAction(ActionEvent event) {
+
     }
 
 }
