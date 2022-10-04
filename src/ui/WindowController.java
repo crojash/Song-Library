cbbe566f24d274e14057d168dbc9d303b959acf4
crojash <crojasherrera1@gmail.com>
2022-10-04 08:51:25 -0400

    Not tableview

--- src/ui/WindowController.java
+++ src/ui/WindowController.java
@@ -2,18 +2,9 @@ package ui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.TableColumn;
 import javafx.scene.control.TextArea;
 
 public class WindowController {
-
-    @FXML
-    private TableColumn<?, ?> artist;
-
-
-    @FXML
-    private TableColumn<?, ?> name;
 
     @FXML
     private TextArea textArea;
