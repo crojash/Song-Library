c864c1a05d438c03fc40aa771a6d3072c50ba438
crojash <crojasherrera1@gmail.com>
2022-10-07 10:07:25 -0400

    Trying a different approach. Using the examples in javafx doc.

--- src/be/Song.java
+++ src/be/Song.java
@@ -1,68 +1,42 @@
 package be;
 
-import javafx.beans.property.IntegerProperty;
-import javafx.beans.property.SimpleIntegerProperty;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.property.StringProperty;
-
 public class Song {
 
-	private StringProperty name = new SimpleStringProperty();
-	private StringProperty artist = new SimpleStringProperty();
-	private StringProperty album = new SimpleStringProperty();
-	private IntegerProperty year = new SimpleIntegerProperty();
+	private String name;
+	private String artist;
+	private String album;
+	private Integer year;
 	
 	//WHen adding the song, name and artist should be entered
 	/*public Song(String name, String artist) {
 		this.name = name;
 		this.artist = artist;
 	}
-	
+	*/
 	public Song(String name, String artist, String album, Integer year) {
 		this.name = name;
 		this.artist = artist;
 		this.album = album;
 		this.year = year;
-	}*/
-
-	/*public Song(String string, String string2, String string3, Integer valueOf) {
-		name.set(string);
-		artist.set(string2);
-		album.set(string3);
-		year.set(valueOf);
-	}*/
-
-	public String getName() {
-		return name.get();
 	}
 
-	public void setName(String name) {
-		this.name.set(name);
+
+	public String getName() {
+		return name;
 	}
 
 	public String getArtist() {
-		return artist.get();
-	}
-
-	public void setArtist(String artist) {
-		this.artist.set(artist);
+		return artist;
 	}
 
 	public String getAlbum() {
-		return album.get();
-	}
-
-	public void setAlbum(String album) {
-		this.album.set(album);
+		return album;
 	}
 
 	public Integer getYear() {
-		return year.get();
+		return year;
 	}
 
-	public void setYear(Integer year) {
-		this.year.set(year);
-	}
 	public String toString() {
 		//Formatted to appear in song detail
 		return String.format("%s, %s, %s, %d. ", name, artist, album, year);
--- src/ui/WindowController.java
+++ src/ui/WindowController.java
@@ -7,8 +7,11 @@ import java.io.FileReader;
 import java.io.IOException;
 
 import be.Song;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.collections.transformation.FilteredList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.ListView;
@@ -22,12 +25,12 @@ public class WindowController {
     private TextArea textArea;
     
     @FXML
-    private ListView<Song> songList;
+    private ListView<?> songList;
     
-    ObservableList<String> test1 = FXCollections.observableArrayList("test1", "test2");
-    ObservableList<String> test2 = FXCollections.observableArrayList("test1.1", "test2.1");
-    ObservableList<Song> songlist = FXCollections.observableArrayList();
-
+    ReadOnlyObjectProperty<ObservableList<Song>> listSongs = new SimpleObjectProperty<>(FXCollections.observableArrayList());
+    ReadOnlyObjectProperty<FilteredList<Song>> viewablelistSongs = new SimpleObjectProperty<FilteredList<Song>>(new FilteredList<>(listSongs.get()));
+    
+    
     @FXML
     void AddOnAction(ActionEvent event) {
 
@@ -51,28 +54,38 @@ public class WindowController {
     @FXML
     private void initialize() throws IOException {
     	//cbSsongs.setItems(test1);
-    	Song song = new Song();
+    	Song[] songs = new Song[0];
+    	int counter = 0;
     	try{
     		BufferedReader reader = new BufferedReader(new FileReader("src/Songs.txt"));
     		String currentL = reader.readLine();
     		
     		while(currentL != null) {
     			
+    			//Extending the array 
+    			Song[] temp = new Song[songs.length + 1];
+    			System.arraycopy(songs, 0, temp, songs.length, counter);
+    			songs = temp;
+    			
+    			
     			String[] line = currentL.split(";");
-    			song.setName(line[0]);
-    			song.setArtist(line[1]);
-    			song.setAlbum(line[2]);
-    			song.setYear(Integer.valueOf(line[3]));
-    			//songlist.add(new Song(line[0], line[1], line[2], Integer.valueOf(line[3])));
-    			System.out.println();
-    			songlist.add(song);
+    			songs[counter] = new Song(line[0], line[1], line[2], Integer.valueOf(line[3]));
+
+    			//songs = new {Song(line[0], line[1], line[2], Integer.valueOf(line[3]};
+    			System.out.println(songs[counter]);
+    			//songlist.add(song);
     			//songList.getItems().addAll(song);
     			currentL = reader.readLine();
+    			counter++;
     		}
     		
     	}catch(FileNotFoundException e) {
     		e.printStackTrace();
     	}
+    	
+    	
+    	
+    	
     	/*songList = new ListView(songlist);*/
     	//songList.setItems(songlist);
     	//songList.getItems().add(songList);
